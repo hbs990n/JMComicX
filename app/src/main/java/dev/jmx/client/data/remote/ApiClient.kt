9f59d74f681e9d9cad1cc78cc01172eeda38b36b
hbs990n <2392159565@qq.com>
2026-06-03 01:20:14 +0800

    fix: add explicit TLS 1.2/1.3 connection spec for Cloudflare compatibility on Android 10

--- app/src/main/java/dev/jmx/client/data/remote/ApiClient.kt
+++ app/src/main/java/dev/jmx/client/data/remote/ApiClient.kt
@@ -1,4 +1,4 @@
-﻿package dev.jmx.client.data.remote
+package dev.jmx.client.data.remote
 
 import dev.jmx.client.data.remote.converter.PrimitiveToRequestBodyConverterFactory
 import dev.jmx.client.data.remote.converter.ResponseConverterFactory
@@ -10,10 +10,13 @@ import dev.jmx.client.storage.CookieStorage
 import dev.jmx.client.task.AppInitTask
 import dev.jmx.client.task.AppTaskInfo
 import dev.jmx.client.utils.log
+import okhttp3.CipherSuite
+import okhttp3.ConnectionSpec
 import okhttp3.Cookie
 import okhttp3.CookieJar
 import okhttp3.HttpUrl
 import okhttp3.OkHttpClient
+import okhttp3.TlsVersion
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
@@ -54,6 +57,22 @@ class ApiClient(
             .connectTimeout(30, TimeUnit.SECONDS)
             .readTimeout(30, TimeUnit.SECONDS)
             .writeTimeout(30, TimeUnit.SECONDS)
+            .connectionSpecs(
+                listOf(
+                    ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
+                        .tlsVersions(TlsVersion.TLS_1_2, TlsVersion.TLS_1_3)
+                        .cipherSuites(
+                            CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+                            CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+                            CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+                            CipherSuite.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+                            CipherSuite.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+                            CipherSuite.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256
+                        )
+                        .build(),
+                    ConnectionSpec.CLEARTEXT
+                )
+            )
             .addInterceptor(initInterceptor)
             .addInterceptor(baseUrlInterceptor)
             .addInterceptor(tokenInterceptor)
